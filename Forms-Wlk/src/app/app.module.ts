b778abb481c56c24970abf92c417acb824dd6747
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-04-03 23:53:10 -0300

    upd forms

--- Forms-Wlk/src/app/app.module.ts
+++ Forms-Wlk/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModalFormComponent } from './modal-form/modal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModalFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
