30401396658af1db2c93b0979f4e7e45dd1d912e
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-04-17 16:31:21 -0300

    add logic

--- ProgramacionReactiva-Wlk/src/app/services/data.service.ts
+++ ProgramacionReactiva-Wlk/src/app/services/data.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { students } from 'src/students';
+import { Student } from '../models/student';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  public studentList = students;
 
-  constructor() { }
+  public studentsPromise!: Promise<Student[]>;
+  public students$!: Observable<Student[]>;
+
+  constructor() {
+    this.studentsPromise = new Promise((res, rej) => {
+      // if (this.studentList.length > 0) {
+      //   res(this.studentList);
+      // } else {
+      //   rej(this.studentList);
+      // }
+      this.studentList.length > 0 ? res(this.studentList) : rej([]);
+    });
+
+    this.students$ = new Observable((res) => {
+      if (this.studentList.length > 0) {
+        res.next(this.studentList);
+        res.complete();
+      } else {
+        res.error('No Data Avalaible');
+      }
+    });
+  }
+
+  GetPromise(): Promise<Student[]> {
+    return this.studentsPromise;
+  }
+
+  GetObservable(): Observable<Student[]> {
+    return this.students$;
+  }
+
+  GetFilter() {
+    return this.students$.pipe(
+      map((curso:Student[]) => curso.filter(person => person.Class > 60))
+    );
+  }
 }
