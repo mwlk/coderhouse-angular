6d43b57c42a0f6a9327944a098e17664b487cdf4
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-04-28 23:26:38 -0300

    add login form

--- 2PF-Wlk/src/app/auth-module/auth/auth.module.ts
+++ 2PF-Wlk/src/app/auth-module/auth/auth.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from '../login/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialHelperModule } from 'src/app/shared-module/material-helper.module';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AuthRoutingModule,
+    MaterialHelperModule
   ]
 })
 export class AuthModule { }
--- 2PF-Wlk/src/app/auth-module/login/login/login.component.ts
+++ 2PF-Wlk/src/app/auth-module/login/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  formLogin: FormGroup;
+  constructor() {
+    this.formLogin = this.prepareForm();
   }
 
+  prepareForm(): FormGroup {
+    return new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {}
+  login() {}
 }
