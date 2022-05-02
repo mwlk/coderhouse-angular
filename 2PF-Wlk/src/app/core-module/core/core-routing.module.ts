aea45349768fe57b641470c403ca0d18d79e8f2b
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-05-01 23:33:19 -0300

    implement guards

--- 2PF-Wlk/src/app/core-module/core/core-routing.module.ts
+++ 2PF-Wlk/src/app/core-module/core/core-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppGuard } from 'src/app/auth-module/auth/guard/app.guard';
 import { TableComponent } from 'src/app/shared-module/components/table/table.component';
 import { AddStudentComponent } from '../add-student/add-student.component';
 import { ListadoGeneralComponent } from '../general/listado-general/listado-general.component';
@@ -8,13 +9,16 @@ const routes: Routes = [
   {
     path: '',
     component: TableComponent,
+    canActivate: [AppGuard]
   },
   {
     path: 'agregar',
     component: AddStudentComponent,
-  },{
+    canActivate: [AppGuard]
+  },{ 
     path: 'general',
-    component: ListadoGeneralComponent
+    component: ListadoGeneralComponent,
+    canActivate: [AppGuard]
   }
 ];
 
